e3cd4d37a4154678f9562a3617a987c685e883c8
Digidolli <iamsadekaye@gmail.com>
2022-02-28 20:24:08 +0000

    added footer note

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import "./index.css"
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode> 
     <App />
+    <footer>
+        This project was coded by <a href="https://www.instagram.com/digidolli/" target="_blank" rel="noreferrer">digidolli</a> and is <a href="https://github.com/digidolli/react-8-ball-app" target="_blank" rel="noreferrer"> open-sourced on Github </a> 
+      </footer>
   </React.StrictMode>,
   document.getElementById('root')
 );
