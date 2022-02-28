0a2f3d0233134a5e5aad2af01450d023c28a47b7
Digidolli <iamsadekaye@gmail.com>
2022-02-28 19:25:36 +0000

    Creates React file, added form, submit and answers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+
+export function App() {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const answers = [
+  "Or nah?",
+  "Hell nah",
+  "Check the mirror lol",
+  "You wish",
+  "This is why you don't have friends",
+  "Duhh??",
+  "I dont care, and neither do you",
+  "Stop asking stupid questions",
+  "Take a shot and try again",
+  "Not in your lifetime",
+  "So now you need my help?",
+  "My reply is no",
+  "Let's sleep on it. Goodnight xoxo 💤",
+  "#doubt",
+  "Empty head, no thoughts. Empty ball, no answer....",
+  "Is that what you realllly what to ask me?",
+  "Stop thinking, start live, laugh, loving",
+  "Dear god no",
+  "The sky is the limit! (You dont have wings, so...)",
+  "That question is triggering, ask me something else"
+];
+
+  const getAnswer = (e) => {
+    e.preventDefault();
+    if (!question) {
+      return;
+    }
+    const index = Math.floor(Math.random() * answers.length);
+    setAnswer(answers[index]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ball">
+      <form onSubmit={getAnswer}>
+        <div>
+          <label>question</label>
+          <input
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+          />
+        </div>
+        <button type="submit">get answer</button>
+      </form>
+      <div className="circle">
+        <p>{answer}</p>
+      </div>
     </div>
   );
 }
